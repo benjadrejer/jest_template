97584ff8d74f94651a7148eea64c3638f01e20df
Benjamin Drejer <benjamin.andreas.drejer@lego.com>
2022-11-29 09:52:26 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/test.test.ts

---
+++ src/checkout.ts
@@ -0,0 +1,6 @@
+export const checkout = (books: string[]) => {
+  if (!books.length) {
+    return 0;
+  }
+  return 8;
+};
--- src/test.test.ts
+++ src/test.test.ts
@@ -1,5 +1,13 @@
-describe("the Jest setup", () => {
-  it("has failing tests", () => {
-    expect(false).toBe(true);
+import { checkout } from "./checkout";
+
+describe("Potter Kata", () => {
+  it("Costs 0 if no books are provided", () => {
+    const result = checkout([]);
+    expect(result).toBe(0);
+  });
+
+  it("Costs 8 if 1 book is provided", () => {
+    const result = checkout(["Potter 1"]);
+    expect(result).toBe(8);
   });
 });
